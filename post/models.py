b808c96c579bbbc0bd2853377e6f5724f28f8945
will-ing <will-ing@github.com>
2020-09-09 20:57:34 -0700

    trouble with docker

--- post/models.py
+++ post/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 class Posts(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    isbn = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=13)
     released = models.CharField(max_length=20)
 
     def __str__(self):
